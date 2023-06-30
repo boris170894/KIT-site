d5279c8773f6457d6ec1cc956fb7ce3e47eaa07e
boris170894 <boris.g170894@gmail.com>
2023-06-30 16:57:59 +0600

    1

--- app/settings.py
+++ app/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lgrf$8=_@3zf-&j-@v)ma%aw&2sz%ll)mov(&#5yp1s^-a7_0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -92,8 +92,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
+        """ 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+         """
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kit',
+        'USER': 'kit',
+        'PASSWORD': '8gPpL0ENQxNQ',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
@@ -141,8 +150,8 @@ LANGUAGES = [
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
-""" STATIC_ROOT = os.path.join(BASE_DIR, 'static') """
+""" STATICFILES_DIRS = [STATIC_DIR] """
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
