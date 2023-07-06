442d07394b8c604330247b57f22ad9e0f5738e8d
vaseles <vaselesstolarov813@gmail.com>
2023-07-06 14:17:14 +0600

    change home page view

--- main/views.py
+++ main/views.py
@@ -6,16 +6,20 @@ from specialties.models import SpecInfoModel
 
 
 def index(request):
-    last_news = NewsModel.objects.filter(news_is_published = True).order_by('news_create_date')[:3]
+    last_news = NewsModel.objects.filter(news_is_published = True).order_by('news_create_date')[1:4]
     achivments = NewsModel.objects.filter(news_is_achivment = True).order_by('news_create_date')[:4]
     contacts = CollegeContactModel.objects.all()
     partners = CollegePartnersModel.objects.all()
+    
+    last_one_news = NewsModel.objects.filter(news_is_published = True).order_by('news_create_date')[0]
 
     context = {
         'last_news': last_news,
         'contacts' : contacts,
         'partners' : partners,
         'achivments' : achivments,
+        
+        'last_one_news': last_one_news,
     }
     return render(request, 'main/pages/index.html', context)
 
